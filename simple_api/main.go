ee131e80b91fb928ad51dbc9c3f8dd950ad5c424
Owen-Choh <94509487+Owen-Choh@users.noreply.github.com>
2024-11-24 15:16:39 +0800

    added simple http server

--- simple_api/main.go
+++ simple_api/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
-func main()  {
-	fmt.Println("Hello world")
+func main()  {	
+	// HTTP request multiplexer to match url of requests
+	router := http.NewServeMux()
+
+	// sample route
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("received request at /")
+		fmt.Fprintf(w,"Hello world!")
+	})
+
+	// set server and start
+	server:= http.Server{
+		Addr: ":8080",
+		Handler: router,
+	}
+	fmt.Println("Starting server on :8080...")
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
+
 }
