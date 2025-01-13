3834223eb6a2e2ce93a886a8628637197391a691
Owen-Choh <94509487+Owen-Choh@users.noreply.github.com>
2025-01-13 16:10:09 +0800

    the index file can perform cors access to the go server

--- simple_api/main.go
+++ simple_api/main.go
@@ -12,6 +12,17 @@ func main()  {
 
 	router := todo.SetUpHttpMux()
 
+	router.HandleFunc("OPTIONS /cors", func(w http.ResponseWriter, r *http.Request){
+		fmt.Println("cors request")
+		fmt.Println(r.Header)
+		
+		w.Header().Add("Access-Control-Allow-Origin","*")
+		w.Header().Add("Access-Control-Allow-Methods","GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS")
+		w.Header().Add("Access-Control-Allow-Headers","Content-Type")
+		w.Header().Add("Access-Control-Max-Age","10000")
+		fmt.Fprint(w, "test cors request success")
+	})
+
 	// set server and start
 	server:= http.Server{
 		Addr: ":8080",
